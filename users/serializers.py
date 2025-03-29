e061602337b1cd012f83d630acb1f7171712d6fa
Mara <Zainullin.marat93@yandex.ru>
2025-03-29 08:22:25 +0300

    project5

--- users/serializers.py
+++ users/serializers.py
@@ -7,3 +7,4 @@ class UserSerializer(ModelSerializer):
     class Meta:
         model = User
         fields = "__all__"
+        extra_kwargs = {'password': {'write_only': True}}  # пароль только для записи
